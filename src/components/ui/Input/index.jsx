8f548d3594fe4c7629e1ce4ff7014ff9d16acafe
Mrfahmii <142069246+Mrfahmii@users.noreply.github.com>
2024-07-19 19:44:34 +0700

    feat:registerpage

--- src/components/ui/Input/index.jsx
+++ src/components/ui/Input/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const Input = ({ label, value, onChange, type = 'text', className }) => {
+const Input = ({ label, value, onChange, type = 'text', name, className }) => {
   return (
     <div className={`flex flex-col ${className}`}>
-      <label className="mb-1">{label}</label>
+      {label && <label className="mb-1">{label}</label>}
       <input
         type={type}
         value={value}
         onChange={onChange}
+        name={name}
         className="border rounded p-2"
       />
     </div>
