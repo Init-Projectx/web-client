b392c430986d10360d73266c5f9e73ed84bd7bdc
Azmi117 <131350266+Azmi117@users.noreply.github.com>
2024-07-20 14:59:56 +0700

    Feature/cart (#7)
    * feat: login
    
    * feature/login
    
    * Feature/ManualPayment
    
    * Revert "Merge branch 'main' of https://github.com/Init-Projectx/Web-client into feature/cart"
    
    This reverts commit 5b1746b97ce2ef7879d4ebccceae71ac84faa808, reversing
    changes made to fe7c024f9915d2e3cb948dfbdc0d9276ad1cee3d.
    
    * Feature/cart
    
    ---------
    
    Co-authored-by: azmiyushari <azmiyushrr@gmail.com>

--- src/components/ui/FileUpload/index.jsx
+++ src/components/ui/FileUpload/index.jsx
@@ -7,10 +7,15 @@ const FileUpload = ({ onDrop, className }) => {
   return (
     <div
       {...getRootProps({ className })}
-      className={`border-dashed border-2 border-gray-300 p-4 ${className}`}
+      className={`border-dashed border-2 border-gray-300 p-4 flex ${className}`}
     >
       <input {...getInputProps()} />
-      <p>Drag 'n' drop some files here, or click to select files</p>
+      <div className='flex flex-col mt-2'>
+        <p>Drag 'n' drop some files here, or click to select files</p>
+        <div className='w-full flex justify-center mt-2'>
+          <p className='text-2xl'>+</p>
+        </div>
+      </div>
     </div>
   );
 };
