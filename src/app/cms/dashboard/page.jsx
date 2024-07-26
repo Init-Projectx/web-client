59e79175a3a4a8776dd0d970e0f5a7586ca600ce
Mahen <166118742+bebek-goreng@users.noreply.github.com>
2024-07-26 20:48:31 +0700

    Feature/fetch cms dashboard (#25)
    * feat: landing page
    
    * edit: feat cart
    
    * feat: checkout page
    
    * update: idr price & register
    
    * feat: cms dashboard

--- src/app/cms/dashboard/page.jsx
+++ src/app/cms/dashboard/page.jsx
@@ -1,11 +1,13 @@
 import LayoutDashboard from "@/components/layout/cms/dashboard";
 
 const CmsDashboard = () => {
-    return(
-        <>
-            <LayoutDashboard/>
-        </>
-    )
-}
+  return (
+    <>
+      <LayoutDashboard />
+    </>
+  );
+};
+
 
 export default CmsDashboard;
+
--- src/components/layout/cms/dashboard/index.jsx
+++ src/components/layout/cms/dashboard/index.jsx
@@ -1,87 +1,68 @@
+"use client";
+
 import Dashboard from "@/components/view/cms/dashboard";
+import Link from "next/link";
 
 const LayoutDashboard = () => {
-    return (
-        <>
-            <div className="h-full flex flex-row">
-            <div className="w-1/5 h-6/6 flex flex-col shadow-lg px-3">
-                <button className="w-full mt-10 flex items-center rounded-md hover:bg-gray-200">
-                    <img src="https://www.svgrepo.com/show/449701/dashboard.svg" className="mx-5 my-1 poppins-bold" alt="" width="25px" height="25px" />
-                    User Dashboard
-                </button>
-                <button className="w-full mt-7 flex items-center rounded-md hover:bg-gray-200">
-                    <img src="https://www.svgrepo.com/show/449762/gift.svg" className="mx-5 my-1 poppins-bold" alt="" width="25px" height="25px" />
-                    Add Product
-                </button>
-                <button className="w-full mt-7 flex items-center rounded-md hover:bg-gray-200">
-                    <img src="https://www.svgrepo.com/show/449846/money-bill.svg" className="ms-5 my-1 poppins-bold" alt="" width="25px" height="25px" />
-                    <p className="ms-1">Payment Confirmation</p>
-                </button>
-                <button className="w-full mt-7 flex items-center rounded-md hover:bg-gray-200">
-                    <img src="https://www.svgrepo.com/show/449902/shopping-cart.svg" className="mx-5 my-1 poppins-bold" alt="" width="25px" height="25px" />
-                    Orders
-                </button>
-            </div>
-            <div className="w-full product-list grid lg:grid-cols-5 md:grid-cols-5 sm:grid-cols-5 gap-6 mx-10 my-8 text-xs md:text-sm sm:text-sm">
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-                <Dashboard />
-            </div>
+  return (
+    <>
+      <div className="h-full flex flex-row">
+        <div className="w-1/5 h-6/6 flex flex-col shadow-lg px-3 text-sm font-bold">
+          <Link href={"/"}>
+            <button className="w-full mt-10 flex items-center rounded-md hover:bg-gray-200">
+              <img
+                src="https://www.svgrepo.com/show/449701/dashboard.svg"
+                className="mx-5 my-1 poppins-bold"
+                alt=""
+                width="25px"
+                height="25px"
+              />
+              User Dashboard
+            </button>
+          </Link>
+          <Link href={"/cms/add-product"}>
+            <button className="w-full mt-7 flex items-center rounded-md hover:bg-gray-200">
+              <img
+                src="https://www.svgrepo.com/show/449762/gift.svg"
+                className="mx-5 my-1 poppins-bold"
+                alt=""
+                width="25px"
+                height="25px"
+              />
+              Add Product
+            </button>
+          </Link>
+          <Link href={"/cms/confirmation"}>
+            <button className="w-full mt-7 flex items-center rounded-md hover:bg-gray-200">
+              <img
+                src="https://www.svgrepo.com/show/449846/money-bill.svg"
+                className="ms-5 my-1 poppins-bold"
+                alt=""
+                width="25px"
+                height="25px"
+              />
+              <p className="ml-5">Payment</p>
+            </button>
+          </Link>
+          <Link href={"/cms/orders"}>
+            <button className="w-full mt-7 flex items-center rounded-md hover:bg-gray-200">
+              <img
+                src="https://www.svgrepo.com/show/449902/shopping-cart.svg"
+                className="mx-5 my-1 poppins-bold"
+                alt=""
+                width="25px"
+                height="25px"
+              />
+              Orders
+            </button>
+          </Link>
         </div>
-        </>
-    );
-}
+        <div className="w-full px-5 py-5">
+          <Dashboard />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default LayoutDashboard;
