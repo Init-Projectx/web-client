0e8c755f8c798b61104272b46cdf06c2daa27e83
azmiyushari <azmiyushrr@gmail.com>
2024-08-01 21:56:32 +0700

    Revert "fixUserProfilPage (#31)"
    This reverts commit b75eadbbb008490dbe14ec3add992fd87338b468.

--- src/components/layout/profile/index.jsx
+++ src/components/layout/profile/index.jsx
@@ -1,20 +1,17 @@
-"use client";
-
 import { useState, useEffect } from 'react';
 import { getUser, updateUser } from '@/modules/fetch/fetchUser.js';
 import styles from "./profile.module.css";
 import FileUpload from "@/components/ui/FileUpload";
 import Button from "@/components/ui/Button";
-import jwtDecode from "jwt-decode";
+import jwtDecode from 'jwt-decode';
 
 const Profile = () => {
     const [userData, setUserData] = useState({
-        id: '',
         username: '',
-        city_id: '',
-        zipCode: '',
-        phoneNumber: '',
-        province_id: '',
+        city: '',
+        zipcode: '',
+        phone: '',
+        province: '',
         address: '',
         photo: null
     });
@@ -25,16 +22,7 @@ const Profile = () => {
                 const token = localStorage.getItem('token');
                 const user = jwtDecode(token);
                 const data = await getUser(user.id);
-                setUserData({
-                    id: data.id,
-                    username: data.username,
-                    city_id: data.city_id,
-                    zipCode: data.zipCode,
-                    phoneNumber: data.phoneNumber,
-                    province_id: data.province_id,
-                    address: data.address,
-                    photo: data.photo,
-                });
+                setUserData(data.data);
             } catch (error) {
                 console.error("Error fetching user data:", error);
             }
@@ -43,38 +31,23 @@ const Profile = () => {
     }, []);
 
     const handleChange = (e) => {
-        const { name, value, files, type } = e.target;
-        setUserData(prevState => ({
-            ...prevState,
-            [name]: type === 'file' ? files[0] : value
-        }));
-    };
-
-    const handleFileDrop = (acceptedFiles) => {
-        setUserData(prevState => ({
-            ...prevState,
-            photo: acceptedFiles[0]
-        }));
+        const { name, value, files } = e.target;
+        if (files && files.length > 0) {
+            setUserData({ ...userData, [name]: files[0] });
+        } else {
+            setUserData({ ...userData, [name]: value });
+        }
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-
-        const data = {
-            data: {
-                username: userData.username,
-                phoneNumber: userData.phoneNumber,
-                city_id: userData.city_id,
-                province_id: userData.province_id,
-                zipCode: userData.zipCode,
-                address: userData.address,
-                photo: userData.photo
-            },
-            id: userData.id
-        };
+        const formData = new FormData();
+        Object.keys(userData).forEach(key => {
+            formData.append(key, userData[key]);
+        });
 
         try {
-            await updateUser(data, userData.photo);
+            await updateUser(formData);
             alert('User updated successfully');
         } catch (error) {
             console.error("Error updating user data:", error);
@@ -92,73 +65,29 @@ const Profile = () => {
                     <div className="grid grid-cols-2 w-4/6 flex-col justify-end">
                         <div className="w-full flex flex-col">
                             <label className="text-sm ms-9 mt-4" htmlFor="username">Username</label>
-                            <input 
-                                id="username" 
-                                name="username" 
-                                value={userData.username} 
-                                onChange={handleChange} 
-                                className="w-5/6 rounded-lg ms-9 mb-4 border-2 border-slate-950" 
-                                required 
-                            />
+                            <input id="username" name="username" value={userData.username} onChange={handleChange} className="w-5/6 rounded-lg ms-9 mb-4 border-2 border-slate-950" />
                             <label className="text-sm ms-9" htmlFor="city">City</label>
-                            <input 
-                                id="city" 
-                                name="city_id" 
-                                value={userData.city_id} 
-                                onChange={handleChange} 
-                                className="w-5/6 rounded-lg ms-9 mb-4 border-2 border-slate-950" 
-                                required 
-                            />
+                            <input id="city" name="city" value={userData.city} onChange={handleChange} className="w-5/6 rounded-lg ms-9 mb-4 border-2 border-slate-950" />
                             <label className="text-sm ms-9" htmlFor="zipcode">Zip code</label>
-                            <input 
-                                id="zipcode" 
-                                name="zipCode" 
-                                value={userData.zipCode} 
-                                onChange={handleChange} 
-                                className="w-5/6 rounded-lg ms-9 mb-4 border-2 border-slate-950" 
-                                required 
-                            />
+                            <input id="zipcode" name="zipcode" value={userData.zipcode} onChange={handleChange} className="w-5/6 rounded-lg ms-9 mb-4 border-2 border-slate-950" />
                         </div>
                         <div className="w-full flex flex-col">
                             <label className="text-sm ms-7 mt-4" htmlFor="phone">Phone number</label>
-                            <input 
-                                id="phone" 
-                                name="phoneNumber" 
-                                value={userData.phoneNumber} 
-                                onChange={handleChange} 
-                                className="w-5/6 rounded-lg ms-7 mb-4 border-2 border-slate-950" 
-                                required 
-                            />
-                            <label className="text-sm ms-7" htmlFor="province_id">Province</label>
-                            <input 
-                                id="province_id" 
-                                name="province_id" 
-                                value={userData.province_id} 
-                                onChange={handleChange} 
-                                className="w-5/6 rounded-lg ms-7 mb-4 border-2 border-slate-950" 
-                                required 
-                            />
+                            <input id="phone" name="phone" value={userData.phone} onChange={handleChange} className="w-5/6 rounded-lg ms-7 mb-4 border-2 border-slate-950" />
+                            <label className="text-sm ms-7" htmlFor="province">Province</label>
+                            <input id="province" name="province" value={userData.province} onChange={handleChange} className="w-5/6 rounded-lg ms-7 mb-4 border-2 border-slate-950" />
                         </div>
                         <div className="w-full">
                             <div className="mb-5">
                                 <label htmlFor="address" className="ms-9 text-sm">Address</label>
-                                <textarea 
-                                    id="address" 
-                                    name="address" 
-                                    value={userData.address} 
-                                    onChange={handleChange} 
-                                    cols="70" 
-                                    rows="6" 
-                                    className="ms-9 rounded-lg border-2 border-slate-950"
-                                    required
-                                ></textarea>
+                                <textarea id="address" name="address" value={userData.address} onChange={handleChange} cols="70" rows="6" className="ms-9 rounded-lg border-2 border-slate-950"></textarea>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="w-3/5 mx-auto mb-10 text-sm">
                     <p>Photo</p>
-                    <FileUpload onDrop={handleFileDrop} className="flex justify-center mt-2 text-lg" />
+                    <FileUpload name="photo" onChange={handleChange} className="flex justify-center mt-2 text-lg" />
                 </div>
                 <div className="w-3/5 flex justify-evenly items-center mx-auto mb-8">
                     <Button type="button" className="w-20 bg-amber-100 rounded-lg h-8 flex items-center text-sm text-yellow-500 hover:bg-red-300 hover:text-gray-100 poppins-semibold">Cancel</Button>
@@ -167,6 +96,6 @@ const Profile = () => {
             </form>
         </div>
     );
-};
+}
 
 export default Profile;
--- src/modules/fetch/fetchUser.js
+++ src/modules/fetch/fetchUser.js
@@ -1,6 +1,5 @@
 import { userInstance } from "@/libs/axios/axiosInstance";
 
-// Function to get user data
 const getUser = async (id) => {
     try {
         const response = await userInstance.get(`/users`, id);
@@ -11,36 +10,12 @@ const getUser = async (id) => {
     }
 }
 
-// Function to update user data with potential file upload
-const updateUser = async (data, file) => {
+const updateUser = async (data) => {
     try {
-        // Creating a new FormData instance
-        const formData = new FormData();
-        
-        // Appending data fields to FormData
-        formData.append("id", data.id);
-        formData.append("username", data.data.username);
-        formData.append("phoneNumber", data.data.phoneNumber);
-        formData.append("city_id", data.data.city_id);
-        formData.append("province_id", data.data.province_id);
-        formData.append("zipCode", data.data.zipCode);
-        formData.append("address", data.data.address);
-        
-        // Appending file to FormData if it exists
-        if (file) {
-            formData.append("photo", file);
-        }
-
-        // Sending FormData to the server
-        const response = await userInstance.put('users', formData, {
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            }
-        });
-
+        const response = await userInstance.put('users', data);
         return response.data;
     } catch (error) {
-        console.error("Error updating user data:", error.response ? error.response.data : error.message);
+        console.error("Error fetching user data:", error.response ? error.response.data : error.message);
         throw error;
     }
 }
