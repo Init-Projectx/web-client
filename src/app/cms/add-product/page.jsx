4cf764ba51d9d83c82bd9a64102ef9bb7e4b262e
Azmi117 <131350266+Azmi117@users.noreply.github.com>
2024-08-02 17:27:35 +0700

    Feat/add products (#42)
    * feat: landing page
    
    * edit: feat cart
    
    * feat: checkout page
    
    * Feature/cmsDashboard
    
    * update: idr price & register
    
    * feat: loginCms
    
    * Fix login cms
    
    * Feature/UserProfile
    
    * update: landing page & details product
    
    * Feature/Cms_Order
    
    * update: fetch order cms
    
    * new update
    
    * update: details product & cart
    
    * update: fetch cities & fetch provinces
    
    * feat: cms-confirmation
    
    * feat/cms-confirmation
    
    * Revert "fixUserProfilPage (#31)"
    
    This reverts commit b75eadbbb008490dbe14ec3add992fd87338b468.
    
    * Reapply "fixUserProfilPage (#31)"
    
    This reverts commit 0e8c755f8c798b61104272b46cdf06c2daa27e83.
    
    * feat/add-product
    
    ---------
    
    Co-authored-by: mahen <oresuki01@gmail.com>
    Co-authored-by: azmiyushari <azmiyushrr@gmail.com>

--- src/app/cms/add-product/page.jsx
+++ src/app/cms/add-product/page.jsx
@@ -1,10 +1,15 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Select from "react-select";
 import Input from "@/components/ui/Input";
 import FileUpload from "@/components/ui/FileUpload";
 import Button from "@/components/ui/Button";
 import { addProductCms } from "@/modules/fetch/cms/fetchProductCms";
+import { searchCities } from "@/modules/fetch/fetchCity";
+import { getAllCategories } from "@/modules/fetch/fetchCategories";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const AddProductPage = () => {
   const [productData, setProductData] = useState({
@@ -12,20 +17,75 @@ const AddProductPage = () => {
     weight: "",
     price: "",
     sku: "",
+    city: "",
     description: "",
     category: "",
-    category_id: "",  // Tambahkan category_id di sini
-    stock: ""
+    category_id: "",
+    stock: "",
   });
   const [file, setFile] = useState(null);
   const [error, setError] = useState(null);
   const [successMessage, setSuccessMessage] = useState(null);
+  const [city, setCity] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const cityResponse = await searchCities(searchQuery);
+        if (Array.isArray(cityResponse)) {
+          const cityOptions = cityResponse.map(city => ({
+            value: city.name,
+            label: city.name,
+          }));
+          setCity(cityOptions);
+        } else {
+          throw new Error("Invalid response format for cities");
+        }
+
+        const categoryResponse = await getAllCategories();
+        if (Array.isArray(categoryResponse.data)) {
+          const categoryOptions = categoryResponse.data.map(category => ({
+            value: category.id,
+            label: category.name,
+          }));
+          setCategories(categoryOptions);
+        } else {
+          throw new Error("Invalid response format for categories");
+        }
+      } catch (error) {
+        console.log("Error fetching data:", error.message);
+      }
+    };
+
+    fetchData();
+  }, [searchQuery]);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setProductData((prevData) => ({
       ...prevData,
-      [name]: value
+      [name]: value,
+    }));
+  };
+
+  const handleCityChange = (selectedOption) => {
+    setProductData((prevData) => ({
+      ...prevData,
+      city: selectedOption ? selectedOption.value : '',
+    }));
+  };
+
+  const handleCityInputChange = (inputValue) => {
+    setSearchQuery(inputValue);
+  };
+
+  const handleCategoryChange = (selectedOption) => {
+    setProductData((prevData) => ({
+      ...prevData,
+      category_id: selectedOption ? selectedOption.value : '',
+      category: selectedOption ? selectedOption.label : '',
     }));
   };
 
@@ -39,10 +99,11 @@ const AddProductPage = () => {
       weight: "",
       price: "",
       sku: "",
+      city: "",
       description: "",
       category: "",
-      category_id: "",  // Reset category_id juga
-      stock: ""
+      category_id: "",
+      stock: "",
     });
     setFile(null);
     setError(null);
@@ -62,20 +123,25 @@ const AddProductPage = () => {
     try {
       const response = await addProductCms(formData);
       setSuccessMessage("Product added successfully!");
+      toast.success("Product added successfully!");
       setError(null);
-      handleCancel(); // Reset form after successful submission
+      handleCancel();
     } catch (error) {
       setError("Error adding product. Please try again.");
+      toast.error("Error adding product. Please try again.");
       setSuccessMessage(null);
     }
   };
 
   return (
     <div className="flex flex-col items-center">
+      <ToastContainer />
       <div className="w-full max-w-4xl bg-white p-6 rounded-lg shadow-md mt-8">
         <h2 className="text-2xl font-bold mb-6">Add New Product</h2>
         {error && <div className="text-red-500 mb-4">{error}</div>}
-        {successMessage && <div className="text-green-500 mb-4">{successMessage}</div>}
+        {successMessage && (
+          <div className="text-green-500 mb-4">{successMessage}</div>
+        )}
         <form onSubmit={handleSubmit} className="space-y-4">
           <div className="grid grid-cols-2 gap-4">
             <Input
@@ -106,13 +172,33 @@ const AddProductPage = () => {
               onChange={handleInputChange}
               className="w-full"
             />
-            <Input
-              label="Category ID"  // Tambahkan input untuk category_id
-              name="category_id"
-              value={productData.category_id}
-              onChange={handleInputChange}
-              className="w-full"
-            />
+            <div>
+              <label>Category</label>
+              <Select
+                options={categories}
+                value={categories.find(
+                  (option) => option.value === productData.category_id
+                )}
+                onChange={handleCategoryChange}
+                className="w-full mt-1"
+                placeholder="Select a category"
+                isSearchable
+              />
+            </div>
+            <div>
+              <label>City</label>
+              <Select
+                options={city}
+                value={city.find(
+                  (option) => option.value === productData.city
+                )}
+                onChange={handleCityChange}
+                onInputChange={handleCityInputChange}
+                className="w-full mt-1"
+                placeholder="Select a city"
+                isSearchable
+              />
+            </div>
             <Input
               label="Description"
               name="description"
@@ -124,17 +210,17 @@ const AddProductPage = () => {
           <div className="flex flex-col items-center mb-6">
             <FileUpload onDrop={handleFileChange} />
           </div>
-          <div className="flex justify-between space-x-4">
+          <div className="flex justify-evenly px-40">
             <Button
               type="button"
               onClick={handleCancel}
-              className="bg-red-500 text-white rounded px-4 py-2"
+              className="bg-yellow-100 text-yellow-600 rounded px-4 py-2 hover:bg-yellow-200"
             >
               Cancel
             </Button>
             <Button
               type="submit"
-              className="bg-green-500 text-white rounded px-4 py-2"
+              className="bg-yellow-500 text-white rounded px-4 py-2 hover:bg-yellow-600"
             >
               Save
             </Button>
